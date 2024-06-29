3e4fe6b3d2745244b28d32f5fdcd50e86a220b2d
chenhe wu <chenhewu@chenhes-MBP.gateway.sasktel.net>
2024-06-29 16:17:27 -0600

    prototype-2 adding cookies and session and more designed registration

--- index.js
+++ index.js
@@ -2,22 +2,43 @@ import express from "express"
 import ejs from "ejs"
 import axios from "axios"
 import pg from "pg"
+import bcrypt from "bcrypt";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
+import session from "express-session";
+import env from "dotenv";
 
 const app = express()
 const port = 3000
-const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    password: "1201",
-    port: 5432,
-    database: "booknote"
-})
+const saltRounds = 10;
+env.config();
+
+app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: true,
+    })
+);
+
+  const db = new pg.Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+  });
 
 db.connect()
 
 
+
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 async function getBook(key, value, size) {
     try {
@@ -30,14 +51,6 @@ async function getBook(key, value, size) {
     
 }
 
-async function registerUser(username, password) {
-    try {
-        await db.query("INSERT INTO user_info(user_name, password) values($1, $2)", [username, password])
-        return true
-    } catch {
-        return false
-    }
-}
 
 async function insertRecord(imgPath, username, title, content) {
     try {
@@ -54,22 +67,40 @@ async function getAllRecord() {
 }
 
 
-async function deleteRecord(id, uname, pwd) {
-        const auth = await db.query("SELECT * FROM user_record a INNER JOIN user_info b ON a.user_name = b.user_name WHERE a.id = $1 AND b.password = $2 AND b.user_name = $3", [id, pwd, uname])
+async function deleteRecordVerify(id, uname) {
+        const auth = await db.query("SELECT * FROM user_record a INNER JOIN user_info b ON a.user_name = b.user_name WHERE a.id = $1 AND b.user_name = $2", [id, uname])
         if (auth.rows.length === 0) {
             return false
         }
-        await db.query("DELETE from user_record WHERE id = $1", [id])
-        return true
+        return auth.rows[0].password
         
 }
 
-
-async function editRecord(id, uname, pwd, message) {
-    const auth = await db.query("SELECT * FROM user_record a INNER JOIN user_info b ON a.user_name = b.user_name WHERE a.id = $1 AND b.password = $2 AND b.user_name = $3", [id, pwd, uname])
-    if (auth.rows.length === 0) {
+async function deleteRecord(id) {
+    try {
+        await db.query("DELETE FROM user_record WHERE id = $1", [id])
+        return true
+    } catch {
+        console.log("db errer")
         return false
     }
+}
+
+async function editRecordVerify(id, uname) {
+    try {
+        const auth = await db.query("SELECT * FROM user_record a INNER JOIN user_info b ON a.user_name = b.user_name WHERE a.id = $1 AND b.user_name = $2", [id, uname])
+        if (auth.rows.length === 0) {
+            return false
+        }
+        return auth.rows[0].password
+    } catch {
+        console.log("db error")
+        return false
+    }
+}
+
+
+async function insertDBrecord(id, message) {
     if (message.title) {
         await db.query("UPDATE user_record SET title = $1 WHERE id = $2", [message.title, id])
         if (message.content) {
@@ -78,21 +109,75 @@ async function editRecord(id, uname, pwd, message) {
         } else {
             return true
         }
-    } 
+    }
 }
-
-app.get("/", async (req, res) => {
-    const posts = await getAllRecord()
-    res.render("index.ejs", {posts: posts})
+app.get("/register", (req, res) => {
+    res.render("register.ejs")
 })
 
-app.post("/create-note", (req, res) => {
-    res.render("form.ejs")
-    
+app.post("/register", async (req, res) => {
+    const email = req.body.username;
+    const password = req.body.password;
+  
+    try {
+      const checkResult = await db.query("SELECT * FROM user_info WHERE user_name = $1", [
+        email,
+      ]);
+  
+      if (checkResult.rows.length > 0) {
+        req.redirect("/login");
+      } else {
+        bcrypt.hash(password, saltRounds, async (err, hash) => {
+          if (err) {
+            console.error("Error hashing password:", err);
+          } else {
+            const result = await db.query(
+              "INSERT INTO user_info (user_name, password) VALUES ($1, $2) RETURNING *",
+              [email, hash]
+            );
+            const user = result.rows[0];
+            console.log(user)
+            req.login(user, (err) => {
+              res.redirect("/home");
+            });
+          }
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+});
+
+app.get("/", (req, res) => {
+    res.render("login.ejs")
+})
+
+app.post("/create-account", passport.authenticate("local", {
+    successRedirect: "/home",
+    failureRedirect: "/"
+}))
+
+
+app.get("/home", async (req, res) => {
+    if (req.isAuthenticated()) {
+        const posts = await getAllRecord()
+        res.render("index.ejs", {posts: posts})
+      } else {
+        res.redirect("/");
+      }
 })
 
 
-app.post("/submit-notes", async (req, res) => {
+app.get("/create-note", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render("form.ejs")
+    } else {
+        res.redirect("/")
+    }
+})
+
+
+app.post("/create-note", async (req, res) => {
     let response
     const imgPath = await getBook(req.body.key, req.body.value, req.body.size)
     console.log(imgPath)
@@ -103,43 +188,51 @@ app.post("/submit-notes", async (req, res) => {
     }
     console.log(response)
     if (response) {
-        res.redirect("/")
+        res.redirect("/home")
+    //consider delete this as user need to be verified before going here
     } else {
-        res.render("form.ejs", {success: "error inserting, go create account first"})
-    }
-})
-app.post("/create-account", async (req, res) => {
-    res.render("registration.ejs")
-})
-
-app.post("/submit-registration", async (req, res) => {
-    const response = await registerUser(req.body.username, req.body.password)
-    if (response) {
-        res.redirect("/")
-    } else {
-        res.render("registration.ejs", {success: response})
+        res.render("form.ejs", {success: "Error inserting, go create account first"})
     }
 })
 
-app.post("/delete-post", (req, res)=>{
-    res.render("form_del.ejs")
+
+
+app.get("/delete-post", (req, res)=>{
+    if (req.isAuthenticated()) {
+        res.render("form_del.ejs")
+    }else {
+        res.redirect("/")
+    }
 })
 
-app.post("/submit-delete-post", async (req, res) => {
-    const response = await deleteRecord(req.body.post_id, req.body.username, req.body.password)
-    if (response) {
-        res.redirect("/")
-    } else {
-        res.render("form_del.ejs", {success: "Combination Error"})
-    }
+app.post("/delete-post", async (req, res) => {
+    const password = await deleteRecordVerify(req.body.post_id, req.body.username)
+    const userpassword = req.body.password
+    bcrypt.compare(userpassword, password, async (err, valid) => {
+        if (err) {
+            console.error("Error comparing passwords:");
+            res.render("form_del.ejs", {success: "Check you enter correctly formated password"})
+        } else {
+        if (valid) {
+            await deleteRecord(req.body.post_id)
+            res.redirect("/home")
+        } else {
+            res.render("form_del.ejs", {success: "Password Incorrect"})
+        }
+        }
+    });
     
 })
 
-app.post("/edit-post", (req, res) => {
-    res.render("form_edit.ejs")
+app.get("/edit-post", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render("form_edit.ejs")
+    } else {
+        res.redirect("/")
+    }
 })
 
-app.post("/submit-edit-post", async (req, res) => {
+app.post("/edit-post", async (req, res) => {
     let message = {}
     if (req.body.title) {
         message.title = req.body.title
@@ -148,14 +241,62 @@ app.post("/submit-edit-post", async (req, res) => {
         message.content = req.body.content
     }
     console.log(message)
-    const response = await editRecord(req.body.post_id, req.body.username, req.body.password, message)
-    if (response){
-        res.redirect("/")
-    } else {
-        res.render("form_edit.ejs", {success: "Something Went Wrong"})
-    }
+    const userpassword = req.body.password
+    const password = await editRecordVerify(req.body.post_id, req.body.username)
+    bcrypt.compare(userpassword, password, async (err, valid) => {
+        if (err) {
+            console.error("Error comparing passwords:");
+            res.render("form_edit.ejs", {success: "Check you enter correctly formated password"})
+        } else {
+        if (valid) {
+            await insertDBrecord(req.body.post_id, message)
+            res.redirect("/home")
+        } else {
+            res.render("form_edit.ejs", {success: "Password Incorrect"})
+        }
+        }
+    });
 
 })
+
+passport.use(
+    "local",
+    new Strategy(async function verify(username, password, cb) {
+      try {
+        const result = await db.query("SELECT * FROM user_info WHERE user_name = $1 ", [
+          username,
+        ]);
+        if (result.rows.length > 0) {
+          const user = result.rows[0];
+          const storedHashedPassword = user.password;
+          bcrypt.compare(password, storedHashedPassword, (err, valid) => {
+            if (err) {
+              console.error("Error comparing passwords:", err);
+              return cb(err);
+            } else {
+              if (valid) {
+                return cb(null, user);
+              } else {
+                return cb(null, false);
+              }
+            }
+          });
+        } else {
+            return cb("User not found");
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    })
+  );
+
+passport.serializeUser((user, cb) => {
+    cb(null, user);
+  });
+  
+passport.deserializeUser((user, cb) => {
+    cb(null, user);
+});
 
 app.listen(port, ()=>{
     console.log("listening at", port)

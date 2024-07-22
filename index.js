d3f8c1713450efd56362347404d9754c442c65c4
chenhe wu <chenhewu@chenhes-MBP.gateway.sasktel.net>
2024-07-22 17:50:16 -0600

    modified callbackURL

--- index.js
+++ index.js
@@ -446,7 +446,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/booknote",
+      callbackURL: "https://book-notes-na99.onrender.com/auth/google/booknote",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     async (accessToken, refreshToken, profile, cb) => {
